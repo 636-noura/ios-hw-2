fe1e1df47d1bd10cf16448421985088f7bfc6e0b
636-noura <66725060+636-noura@users.noreply.github.com>
2020-06-23 23:53:53 +0300

    done

--- iOSHomework2/ViewController.swift
+++ iOSHomework2/ViewController.swift
@@ -19,8 +19,10 @@ class ViewController: UIViewController {
         let member = nameTextField.text!
         
         // MARK: -   1️⃣ تحت الخط membersNamesArray إلي المصفوفه memberقم بإضافة المتغير
+      
+        membersNamesArray.append(member)
         
-        
+        print(membersNamesArray)
         
         // MARK: -   النهاية
         
@@ -34,7 +36,7 @@ class ViewController: UIViewController {
         
         // MARK: -  4️⃣ functionCall داخل المتغير  secretNameLetter قم باستدعاء الدالة
         
-        var functionCall = ""
+        var functionCall = secretNameLetter(members: membersNamesArray)
         
         
         // MARK: -   النهاية
@@ -52,7 +54,7 @@ class ViewController: UIViewController {
         
         // MARK: -  5️⃣ functionCall داخل المتغير  secretNameEmoji قم باستدعاء الدالة
         
-        var functionCall = ""
+        var functionCall = secretNameEmoji(array: membersNamesArray)
         
         // MARK: -   النهاية
         
@@ -66,7 +68,15 @@ class ViewController: UIViewController {
     
     // MARK: - 2️⃣ تحت هذا الخط secretNameLetter قم بكتابة الداله
     
-    
+    func secretNameLetter(members: [String]) -> String{
+        var secret = ""
+        
+        for member in members{
+            secret += (member.prefix(1)).uppercased()
+        }
+        
+        return secret
+    }
     // MARK: -   النهاية
     
     
@@ -75,10 +85,50 @@ class ViewController: UIViewController {
     
     // MARK: - 3️⃣ تحت هذا الخط secretNameEmoji قم بكتابة الداله
     
+    func secretNameEmoji(array: [String]) -> String{
+        var secret: String = ""
+        let emojiLetter: [String: String] =
+        [
+            "A":"🐶",
+            "B":"🐱",
+            "C":"🐭",
+            "D":"🐹",
+            "E":"🐰",
+            "F":"🦊",
+            "G":"🐻",
+            "H":"🐼",
+            "I":"🐨",
+            "J":"🐯",
+            "K":"🦁",
+            "L":"🐮",
+            "M":"🐷",
+            "N":"🐸",
+            "O":"🐵",
+            "P":"🐔",
+            "Q":"🐧",
+            "R":"🐣",
+            "S":"🦆",
+            "T":"🦅",
+            "U":"🦉",
+            "V":"🦇",
+            "W":"🦋",
+            "X":"🐌",
+            "Y":"🦄",
+            "Z":"🐢",]
     
+    //take first letter from each array and append it to secret
+    
+    for member in array{
+    let prefix = String(member.prefix(1)).uppercased()
+        let emoji = emojiLetter[prefix]!
+        secret += emoji
+    }
+    
+        return secret
+}
     // MARK: -   النهاية
     
-    
-    
 }
+    
+
 
